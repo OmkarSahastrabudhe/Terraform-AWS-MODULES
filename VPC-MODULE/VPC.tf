c37712c128a450660febe4708612b6632a348b21
OmkarSahastrabudhe <omkarsahastrabudheofficial@gmail.com>
2026-02-24 21:56:57 +0530

    commit it

--- VPC-MODULE/VPC.tf
+++ VPC-MODULE/VPC.tf
@@ -34,13 +34,14 @@ resource "aws_default_route_table" "default_routetable" {
         env = var.env
     }
     default_route_table_id = aws_vpc.my_vpc.default_route_table_id
-    route = [
-        {
-            cidr_block = "0.0.0.0/0"
-            gateway_id = aws_internet_gateway.my_igw.id
-        }
-    ]
+   
 }
+resource "aws_route" "default_internet_access" {
+  route_table_id         = aws_default_route_table.default_routetable.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
 
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.my_vpc.id
