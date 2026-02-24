969f77574d4ff8a10e3e649bc7c87d5a89bba505
OmkarSahastrabudhe <omkarsahastrabudheofficial@gmail.com>
2026-02-24 13:29:01 +0530

    commit changes

---
+++ EC2-MODULE/VARIABLES.tf
@@ -0,0 +1,24 @@
+variable "ami_id" {
+
+    description = "The AMI ID to use for the EC2 instance"
+
+  
+}
+variable "key_name" {
+
+    description = "The name of the key pair to use for the EC2 instance"
+  
+}
+
+
+
+
+ 
+  
+
+
+variable "instance_type" {
+
+    description = "The type of instance to use for the EC2 instance"
+  
+}
---
+++ EC2-MODULE/ec2-deploy.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+
+    region = ap-south-1
+  
+}
+
+module "ec2_module" {
+  source = "./EC2-MODULE"
+  ami_id = "ami-019715e0d74f695be"
+  instance_type = "t3.small"
+  key_name = "superkey"
+}
