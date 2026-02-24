c7489030eb60f3ec2a3fe8e7b5a63d9aea04c949
OmkarSahastrabudhe <omkarsahastrabudheofficial@gmail.com>
2026-02-24 23:05:42 +0530

    commit

---
+++ mainvar.tf
@@ -0,0 +1,27 @@
+variable "project_id" {
+  description = "The ID of the project in which to create the resources."
+  type        = string
+  
+}
+variable "env" {
+  description = "The environment in which to create the resources."
+  type        = string
+  
+}
+
+variable "key_name" {
+  description = "The name of the key pair to use for the EC2 instance."
+  type        = string
+  
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance."
+  type        = string
+  
+}
+variable "instance_type" {
+  description = "The instance type to use for the EC2 instance."
+  type        = string
+  
+}
