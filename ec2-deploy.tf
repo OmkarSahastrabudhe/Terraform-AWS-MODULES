3fdf03e47338f16e6a7c6df238cb18a55056aa5e
OmkarSahastrabudhe <omkarsahastrabudheofficial@gmail.com>
2026-02-24 22:27:49 +0530

    commit it

--- ec2-deploy.tf
+++ ec2-deploy.tf
@@ -14,13 +14,35 @@ module "vpc_default" {
 
 module "ec2" {
   source = "./EC2-MODULE"
-  ami_id = "ami-019715e0d74f695be"
-  instance_type = "t3.small"
-  key_name = "superkey"
+  ami_id = var.ami_id
+  instance_type = var.instance_type
+  key_name = var.key_name
   subnet_id_public = module.vpc_default.subnet_id_public
   security_group_id = module.vpc_default.default_securitygroup
-  projectname = "cbz"
-  env = "dev"
+  projectname = var.projectname
+  env = var.env
 
   
+}
+
+variable "instance_type" {
+
+  default = "t3.micro"
+
+  
+}
+variable "ami_id" {
+
+  default = "ami-019715e0d74f695be"
+}
+
+variable "projectname" {
+  default = "cbz"
+}
+variable "env" {
+  default = "dev"
+  
+}
+variable "key_name" {
+  default = "superkey"
 }
